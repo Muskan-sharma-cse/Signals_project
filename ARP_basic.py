1c6a87dbbe30bcf8d1e6d4493ac426cc25b07f87
Muskan-sharma-cse <sharmamahak09069@gmail.com>
2026-09-22 17:50:29 +0530

    Create ARP_basic.py

---
+++ ARP_basic.py
@@ -0,0 +1,12 @@
+
+# 1. Hamari Diary jisme IP aur MAC address safe hain
+my_diary = {"192.168.1.5": "11-22-33-44-55-66"} [9.2]
+
+# 2. Humne computer ko apna IP Address diya 🎯
+give_ip = "192.168.1.5"
+
+# 3. Computer ne diary mein se is IP ka MAC address nikal kar dikha diya
+print("✅ MAC Address mila:", my_diary[give_ip]) [9.2]
+
+output:
+MAC Address mila: 11-22-33-44-55-66
